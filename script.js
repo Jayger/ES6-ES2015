7767db692ff63822aead57c9c2ac3e4dc31bd13f
Jordan R <jaygerzero@gmail.com>
2020-01-11 18:44:20 -0500

    Destructuring

--- script.js
+++ script.js
@@ -126,6 +126,7 @@ console.log(ages6);
 //////////////////////////////
 // Lecture: Arrow Functions 2
 
+/*
 // ES5 
 var box5 = {
     color: 'green',
@@ -202,8 +203,45 @@ Person.prototype. myFriends5 = function(friends) {
 }
 
 new Person('Mike').myFriends5(friends);
+*/
 
+/////////////////////////////////
+// Lecture: Destructuring
+/*
+// ES5
+var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
 
+// ES6
+const [name, age] = ['John', 29];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+
+console.log(age2);
+console.log(retirement);
+ */
 
 
 
