4622fe2e114fb835bb72657fba097a37c14c4d66
Jordan R <jaygerzero@gmail.com>
2020-01-11 19:04:21 -0500

    Arrays

--- script.js
+++ script.js
@@ -243,5 +243,58 @@ console.log(age2);
 console.log(retirement);
  */
 
+/////////////////////////////////
+// Lecture: Arrays
+/*
+const boxes = document.querySelectorAll('.box');
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxes.forEach(function(curr) {
+    curr.style.backgroundColor = 'dodgerblue';
+})
 
 
+//ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+// ES5
+// for(var i = 0; i < boxesArr5.length; i++){
+//     if(boxesArr5[i].className === 'box blue'){
+//         // continue;
+//         break;
+//     }
+
+//     boxesArr5[i].textContent = 'I changed to blue!';
+// }
+
+// ES6
+for(const cur of boxesArr6) {
+    if(cur.className.includes('blue')){
+        continue;
+        // break;
+    };
+    cur.textContent = 'I changed to blue!';
+}
+
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];c
+
+var full = ages.map(function(curr) {
+    return curr >= 18;
+});
+
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+
+console.log(ages.findIndex(cur => cur >= 18));
+
+console.log(ages.find(cur => cur >= 18));
+*/
