987b8c9b1e0d972e57c5a2446a6dd8aee1ea8c48
Jordan R <jaygerzero@gmail.com>
2020-01-12 17:01:46 -0500

    Default Parameters

--- script.js
+++ script.js
@@ -352,7 +352,6 @@ function isFullAge6(...years) {
 }
 
 isFullAge6(1990, 1999, 1965, 2019, 1984);
-*/
 
 // ES5
 function isFullAge5() {
@@ -373,3 +372,37 @@ function isFullAge6(...years) {
 }
 
 isFullAge6(16, 1990, 1999, 1965, 2019, 1984);
+*/
+
+///////////////////////////////////
+// Lecture: Default Parameters
+/*
+// ES5
+// function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+
+//     lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+//     nationality === undefined ? nationality = 'American' : nationality = nationality;
+
+//     this.firstName = firstName;
+//     this.yearOfBirth = yearOfBirth;
+//     this.lastName = lastName;
+//     this.nationality = nationality;
+// };
+
+// var john = new SmithPerson('John', 1990); 
+// var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
+
+// ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'American') {
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
+};
+
+let john = new SmithPerson('John', 1990); 
+let emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
+*/
+
+///////////////////////////////////
+//
